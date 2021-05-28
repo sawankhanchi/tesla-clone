2e0809dab3c6d00c9ed8e4fe96c5e17745b0828c
sawankhanchi <sappler16@gmail.com>
2021-05-28 23:20:50 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import styled from "styled-components";
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
+
+function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+    // console.log(cars);
+
+    return (
+        <Container>
+            {/* logo */}
+            <a href="logo">
+                <img src="/images/logo.svg" alt="" />
+            </a>
+
+            {/* menu left */}
+            <Menu>
+                { cars && cars.map((car, index) => (
+                    <a key={index} href="model">{car}</a>
+                ))}
+            </Menu>
+
+            {/* menu right */}
+            <RightMenu>
+                <p><a href="shop">Shop</a></p>
+                <a href="tacc">Tesla account</a>
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
+            </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CustomClose onClick={() => setBurgerStatus(false)} />
+                { cars && cars.map((car, index) => (
+                    <li key={index}><a href="model">{car}</a></li>
+                ))}
+                <li><a href="ei">Existing Inventory</a></li>
+                <li><a href="ui">Used Inventory</a></li>
+                <li><a href="ti">Trade-in</a></li>
+                <li><a href="ct">Cybertruck</a></li>
+                <li><a href="rs">Roadster</a></li>
+            </BurgerNav>
+            
+        </Container>
+    )
+}
+
+export default Header;
+
+const Container = styled.div`
+   min-height: 60px;
+   position: fixed;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   padding: 0 20px;
+   z-index: 1;
+
+`
+const Menu = styled.div`
+   display: flex;
+   align-items: center;
+   justify-center: center;
+   flex: 1;
+   margin-left: 120px;
+
+   a {
+       font-weight: 600;
+       text-transform: uppercase;
+       padding: 0 10px;
+       flex-wrap: nowrap;
+   }
+
+`
+
+const RightMenu = styled.div`
+   margin-left: 120px;
+   display: flex;
+   align-items: center;
+    
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 25px;
+    }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+
+    p {
+        margin-right: 2px;
+    }
+
+`
+
+const CustomMenu = styled(MenuIcon)`
+   cursor: pointer;
+
+`
+
+const BurgerNav = styled.div`
+   position: fixed;
+   top: 0;
+   bottom: 0;
+   right: 0;
+   background: white;
+   width: 300px;
+   z-index: 16;
+   list-style: none;
+   padding: 20px;
+   display: flex;
+   flex-direction: column;
+   text-align: start;
+   justify-content: flex-start;
+   transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+   transition: transform 0.2s;
+   li {
+       padding: 15px 0;
+       border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+       a {
+           font-weight: 600;
+       }
+   }
+
+`
+
+const CustomClose = styled(CloseIcon)`
+   margin-left: 240px;
+   margin-top: -5px;
+   margin-bottom: 10px;
+   cursor: pointer;
+`
---
+++ src/components/Section.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
+
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
+    return (
+        <Wrap bg={backgroundImg}>
+            <Fade left>
+                <ItemText>
+                    <h1>{ title }</h1>
+                    <p>{ description }</p>
+                </ItemText>
+            </Fade>
+            <Buttons>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            { leftBtnText }
+                        </LeftButton>
+                    
+                        <RightButton>
+                            { rightBtnText }
+                        </RightButton>
+                    </ButtonGroup>
+                </Fade>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>  
+        </Wrap>
+    )
+}
+
+export default Section;
+
+const Wrap = styled.div`
+   width: 100vw;
+   height: 100vh;
+   background-size: cover;
+   background-position: center;
+   background-repeat: no-repeat;
+   background-image: url('/images/model-s.jpg');
+   dispaly: flex;
+   flex-direction: column;
+   justify-content: space-between; // vertical
+   align-items: center; // horizontal
+   background-image: ${props => `url('/images/${props.bg}')`}
+`
+
+const ItemText = styled.div`
+   padding-top: 15vh;
+   text-align: center;
+`
+
+const ButtonGroup = styled.div`
+   display: flex;
+   margin-bottom: 30px;
+   margin-top: 420px;
+   margin-left: 350px;
+   justify-content: space-between;
+   width: 600px;
+   @media (max-width: 768px) {
+       flex-direction: column;
+   }
+
+`
+
+const LeftButton = styled.div`
+   background-color: rgba(23, 26, 32, 0.8);
+   height: 40px;
+   width: 250px;
+   color: white;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   border-radius: 100px;
+   opacity: 0.85;
+   text-transform: uppercase;
+   font-size: 12px;
+   cursor: pointer;
+   margin-left: 72.5px
+
+`
+
+const RightButton = styled.div`
+   background-color: white;
+   color: black;
+   height: 40px;
+   width: 250px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   border-radius: 100px;
+   opacity: 0.75;
+   text-transform: uppercase;
+   font-size: 12px;
+   cursor: pointer;
+   margin-right: 7.5px;
+`
+
+const DownArrow = styled.img`
+   margin-top: -20px;
+   height: 40px;
+   overflow-x: hidden;
+   animation: animationDown infinite 1.5s;
+
+`
+
+const Buttons = styled.div`
+
+`
